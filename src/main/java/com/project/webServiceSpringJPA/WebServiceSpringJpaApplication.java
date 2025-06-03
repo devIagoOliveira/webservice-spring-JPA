b99ad3470975c399a16bd1b215e2e33c312e0867
Iago Oliveira <deviagooliveira@gmail.com>
2025-06-03 15:01:16 -0300

    project created

---
+++ src/main/java/com/project/webServiceSpringJPA/WebServiceSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.project.webServiceSpringJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/webServiceSpringJPA/WebServiceSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.webServiceSpringJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
